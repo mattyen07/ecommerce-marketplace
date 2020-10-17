6f1f043ca0c3381294703ee527c19b311930330e
mpchow <mattchow918@gmail.com>
2020-10-17 15:18:34 -0700

    Initial skeleton

---
+++ frontend/src/pages/createProfile/createProfile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CreateProfile extends React.Component {
+   render() {
+      return (
+         <div>
+
+         </div>
+      );
+   }
+
+}
+
+export default CreateProfile;
