f68c50f66c9d33bf21df39c0d8c9e2d476725f77
huajustin <huajustinh@gmail.com>
2020-10-18 00:00:17 -0700

    Update HTTP response header

--- api/routes/authentication.js
+++ api/routes/authentication.js
@@ -4,6 +4,7 @@ const databaseHelpers = require('../helpers/databaseGet');
 var router = express.Router();
 
 router.post('/', async function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
     let email = req.body.email;
     let password = req.body.password;
 
