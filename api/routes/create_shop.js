8dfe7313e2da90852775aa717f5751b6297fd9c2
huajustin <huajustinh@gmail.com>
2020-10-17 21:40:08 -0700

    Add error handling for create shop endpoint

--- api/routes/create_shop.js
+++ api/routes/create_shop.js
@@ -5,8 +5,21 @@ const router = express.Router();
 
 /* POST create a new shop */
 router.post('/', function(req, res) {
-    databaseHelpers.createUser(req.body.name, req.body.email);
-    databaseHelpers.createShop(req.body);
+    if (databaseHelpers.createUser(req.body.name, req.body.email) == -1) {
+        res.status(400).json({
+            status: 400,
+            message: 'Missing name or email!'
+        });
+        return;
+    };
+
+    if (databaseHelpers.createShop(req.body) == -1) {
+        res.status(400).json({
+            status: 400,
+            message: 'Missing attribute! Require name, ownerName, email, address, phone'
+        });
+        return;
+    };
     res.status(200).send('Success!');
 });
 
