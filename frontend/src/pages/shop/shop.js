0752ab6dfcef9838b4a0992582453e3ac1f4fb0a
Andrea Shao <andrea25shao@gmail.com>
2020-10-18 03:25:48 -0700

    Shop page picture

--- frontend/src/pages/shop/shop.js
+++ frontend/src/pages/shop/shop.js
@@ -4,7 +4,7 @@ import './shop.css'
 class Shop extends React.Component {
    constructor (props) {
       super(props);
-      this.state = {shopName: "Andreas Shop", ownerName: "Andrea Shao", email: "andrea@hotmail.com", address: "111 Andrea St", products: ["Andrea shirt", "andrea pants"]};
+      this.state = {shopName: "Andrea's Candy Shop", ownerName: "Andrea Shao", email: "andrea@hotmail.com", address: "1392 Candy St", products: ["Twizzlers", "Jelly Beans"]};
    }
 
    
@@ -12,14 +12,14 @@ class Shop extends React.Component {
       return (
          <div className="storeContainer">
             <div className="item">
-               <img src={require("./Background3.jpg")}></img>
+               <img src={require("./CandyStore.jpg")} className="item" id="shopImage"></img>
             </div>
             <div className="item">
                <h1>{this.state.shopName}</h1>
                <h1>{this.state.ownerName}</h1>
                <h1>{this.state.email}</h1>
                <h1>{this.state.address}</h1>
-               {this.state.products.map(name => <h1>{name}</h1>)}
+               <button>Return</button>
             </div>
          </div>
       );
