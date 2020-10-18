45921d0bdb7dfb6238998618398b1cdcb131a5e1
mpchow <mattchow918@gmail.com>
2020-10-17 19:02:49 -0700

    Structure for shops page

--- frontend/src/pages/shop/shop.js
+++ frontend/src/pages/shop/shop.js
@@ -3,8 +3,19 @@ import React from 'react';
 class Shop extends React.Component {
    constructor (props) {
       super(props);
-      this.state = {};
+      this.state = {shopeName: "", ownerName: "", email: "", address: "", products: []};
+      this.getShop = this.getShop.bind(this);
+   }
 
+   getShop = async () => {
+      try {
+         const response = await fetch('');
+         const json = await response.json();
+         this.setState(state => {shopData});
+      }
+      catch(e) {
+         console.log(e);
+      }
    }
 
 
