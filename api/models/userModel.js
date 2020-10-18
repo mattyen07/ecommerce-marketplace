dd7377edb930e0b5a82826d4337aade54df28368
huajustin <huajustinh@gmail.com>
2020-10-18 01:09:17 -0700

    Add storeOwner boolean field to user models

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
     name: String,
     email: String,
     // this is what happens when we have only a few hours to finish our project and no time to implement proper authorization
-    password: String
+    password: String,
+    storeOwner: Boolean
 });
 
 userSchema.methods.getName = function() {
