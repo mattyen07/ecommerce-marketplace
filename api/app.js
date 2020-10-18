4b2f3117be6c6da65c47286cf68c6a893678dba4
huajustin <huajustinh@gmail.com>
2020-10-18 00:29:07 -0700

    Fix CORS policy

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 const { ExpressPeerServer } = require('peer');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
